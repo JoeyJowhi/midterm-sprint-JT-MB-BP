39aeec3e30a8e6303ae6ff1cc2374483f9971a99
Joey <jowhiey@gmail.com>
2025-06-17 11:47:40 -0230

    Accidentally committed without adding a message oopsie lol, Joey finished Medication class and started a little on testing its methods.

--- src/PharmacySystem/Medication.java
+++ src/PharmacySystem/Medication.java
@@ -1,19 +1,73 @@
 package PharmacySystem;
-
 import java.time.LocalDate;
 
 public class Medication {
-    private String id;
-    private String name;
-    private String dose;
+    private final int id;
     private int quantityInStock;
+    private String name, dose;
     private LocalDate expiryDate;
-}
 
-public Medication (String id, String name, String dose, int quantityInStock){
-    this.id = id;
-    this.name = name;
-    this.dose = dose;
-    this.quantityInStock = quantityInStock;
-    this.expiryDate = ;
+    static int count = 1;
+
+
+    //Constructors
+    public Medication() {
+        this.id = count;
+        this.name = null;
+        this.dose = null;
+        this.quantityInStock = 0;
+        this.expiryDate = null;
+
+        count++;
+    }
+
+    public Medication(String name, String dose, int quantityInStock, LocalDate expiryDate) {
+        this.id = count;
+        this.name = name;
+        this.dose = dose;
+        this.quantityInStock = quantityInStock;
+        this.expiryDate = expiryDate;
+
+        count++;
+    }
+
+
+    //Getter Methods
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDose() {
+        return this.dose;
+    }
+
+    public int getQuantityInStock() {
+        return this.quantityInStock;
+    }
+
+    public LocalDate getExpiryDate() {
+        return this.expiryDate;
+    }
+
+
+    //Setter Methods
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDose(String dose) {
+        this.dose = dose;
+    }
+
+    public void setQuantityInStock(int quantityInStock) {
+        this.quantityInStock = quantityInStock;
+    }
+
+    public void setExpiryDate(LocalDate expiryDate) {
+        this.expiryDate = expiryDate;
+    }
 }
--- src/PharmacySystem/TestPharmacySystem.java
+++ src/PharmacySystem/TestPharmacySystem.java
@@ -1,4 +1,8 @@
 package PharmacySystem;
 
 public class TestPharmacySystem {
+    public static void main(String[] args) {
+        //Medication Class Tests
+        Medication joeynnol = new Medication();
+    }
 }
