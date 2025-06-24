23976c8a55247f0cbf178f8e9af429111591bc60
MikeBarney88 <michaelbarney1988@gmail.com>
2025-06-24 11:22:33 -0230

    nothing changed.

--- src/PharmacySystem/TestPharmacySystem.java
+++ src/PharmacySystem/TestPharmacySystem.java
@@ -3,19 +3,19 @@ import java.time.LocalDate;
 
 public class TestPharmacySystem {
     public static void main(String[] args) {
-    //Medication Class Tests
+        //Medication Class Tests
         //Constructor Tests (Also tests toString() by proxy)
         Medication joeynnol = new Medication();
         System.out.println("Medication Class Tests\n   Constructor Tests\n" + joeynnol + "\n");
 
         Medication joeymine = new Medication("Joeymine", "13mg", 16, LocalDate.parse("2025-06-17"));
         System.out.println(joeymine + "\n\n   Getter Method Tests\n");
-        
-        
+
+
         //Getter Method Tests
         System.out.println("getID(): " + joeymine.getId() + "\ngetName(): " + joeymine.getName() + "\ngetDose(): " + joeymine.getDose() + "\ngetQuantityInStock(): " + joeymine.getQuantityInStock() + "\ngetExpiryDate(): " + joeymine.getExpiryDate() + "\n\n   Setter Method Tests\n");
-        
-        
+
+
         //Setter Method Tests
         joeymine.setName("Joeycyline");
         joeymine.setDose("10mg");
@@ -25,7 +25,7 @@ public class TestPharmacySystem {
         System.out.println("--After Setters--\n" + joeymine);
 
 
-    //MedicationTrackingSystem Tests
+        //MedicationTrackingSystem Tests
         //List Manipulation Method Tests
         MedicationTrackingSystem.addToMedicationInventory(joeynnol);
         MedicationTrackingSystem.addToMedicationInventory(joeymine);
